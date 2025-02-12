d65a907738dfe002e442515371c0750453cc399a
minford <mf557@byu.edu>
2025-02-12 01:23:34 -0700

    remove example

--- eslintExample/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-
-export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
-  { languageOptions: { globals: globals.jest } },
-  pluginJs.configs.recommended,
-];
